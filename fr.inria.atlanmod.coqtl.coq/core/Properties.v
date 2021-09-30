0202e7b2893d31d10464cfcf87e21874aea83afb
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2021-09-30 13:50:17 +0200

    first try

--- fr.inria.atlanmod.coqtl.coq/core/Properties.v
+++ fr.inria.atlanmod.coqtl.coq/core/Properties.v
@@ -25,25 +25,109 @@ forall (tc: TransformationConfiguration) (f: SourceModel -> TargetModel),
   forall (sm: SourceModel), allModelElements (execute t sm) = allModelElements (f sm).
 Proof.
   intros.
-  exists (buildTransformation 0 
+  pose (buildTransformation 0 
     ((buildRule "rule"%string 
      (fun sm sp => match sp with nil => Some true | _ => Some false end)
-     (fun sm sp => Some (length (allModelElements (f sm))))
       (buildOutputPatternElement "out"%string 
+      (fun sm sp => Some (length (allModelElements (f sm))))
       (fun i sm sp => nth_error (allModelElements (f sm)) i)
       nil 
       :: nil))
      ::nil)).
+  exists t.
   intros.
   unfold execute. 
   unfold instantiatePattern. 
   unfold instantiateRuleOnPattern. 
-  unfold instantiateIterationOnPattern. 
-  unfold evalIteratorExpr. 
+  unfold instantiateElementsOnPattern. 
+  unfold evalElementIteratorExpr. 
   simpl.
+  repeat rewrite app_nil_r.
   destruct (f sm).
   induction modelElements.
   * reflexivity.
+  * simpl.
+    f_equal.
+    rewrite <- IHmodelElements at 2.
+    repeat rewrite flat_map_concat_map.
+    f_equal.
+    rewrite map_map.
+    reflexivity.
+Qed.
+
+Theorem universality_links :
+forall (tc: TransformationConfiguration) (f: SourceModel -> TargetModel),
+  exists (t: Transformation), 
+  forall (sm: SourceModel), allModelElements (f sm) <> nil -> allModelLinks (execute t sm) = allModelLinks (f sm).
+Proof.
+  intros.
+  pose (buildTransformation 0 
+    ((buildRule "rule"%string 
+     (fun sm sp => match sp with nil => Some true | _ => Some false end)
+      (buildOutputPatternElement "out"%string 
+      (fun sm sp => Some (length (allModelElements (f sm))))
+      (fun i sm sp => nth_error (allModelElements (f sm)) i)
+      ((buildOutputPatternLink 
+        (fun tls i sm sp te => match i with | 0 => Some (length (allModelLinks (f sm))) | _ => None end)
+        (fun il tls i sm sp te => nth_error (allModelLinks (f sm)) il)
+      )::nil) 
+      :: nil))
+     ::nil)).
+  exists t.
+  intros.
+  unfold execute. 
+  unfold applyPattern. 
+  unfold applyRuleOnPattern. 
+  unfold applyElementsOnPattern. 
+  unfold applyElementOnPattern. 
+  unfold applyLinksOnPattern.
+  unfold applyLinkOnPattern.
+  unfold evalOutputPatternLinkExpr.
+  unfold evalElementIteratorExpr.
+  unfold evalLinkIteratorExpr. 
+  unfold evalExpr.
+  simpl.
+  destruct (f sm). simpl.
+  destruct modelElements.
+  * simpl. contradiction.
+  * simpl. 
+    repeat rewrite app_nil_r.
+    assert (flat_map (fun n : nat => optionToList (nth_error modelLinks n))
+    (indexes (Datatypes.length modelLinks)) = modelLinks). {
+      clear H.
+      induction modelLinks.
+      + reflexivity.
+      + simpl.
+        f_equal.
+        rewrite <- IHmodelLinks at 2.
+        repeat rewrite flat_map_concat_map.
+        f_equal.
+        rewrite map_map.
+        reflexivity.
+    }
+    rewrite <- H0 at 2.
+    rewrite app_nil_end.
+    f_equal.
+    apply in_flat_map_nil.
+    intros.
+    repeat rewrite app_nil_r.
+    destruct a.
+    + exfalso.
+      Search map S.
+
+  
+  induction modelLinks.
+  * 
+    Search flat_map.
+    apply in_flat_map_nil.
+    intros.
+    rewrite app_nil_r.
+    simpl.
+    destruct (nth_error modelElements a).
+    + apply in_flat_map_nil.
+      intros.
+      destruct a; reflexivity.
+    + reflexivity.
   * simpl.
     f_equal.
     rewrite <- IHmodelElements at 2.
