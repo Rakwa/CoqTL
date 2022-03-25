3d4be5078286c71a748b8b7d6ffa94d619c09610
Zheng Cheng <cz0590@gmail.com>
2022-03-25 20:33:23 +0100

    sketch proof for moore2mealy eq

--- transformations/Moore2Mealy/Equivalence.v
+++ transformations/Moore2Mealy/Equivalence.v
@@ -6,19 +6,30 @@ Require Import MealySemantics.
 Require Import Moore2Mealy.
 Require Import core.Semantics.
 Require Import core.utils.CpdtTactics.
-
-
+Require Import List.
 Scheme Equality for list.
+
+
+
 
 Theorem Moore2Mealy_equivalence :
     forall (m : MooreModel) (i: list string), 
         Moore_execute m i = Mealy_execute (execute Moore2Mealy m) i.
 Proof.
-unfold Moore_execute, Mealy_execute. 
-induction i.
-- simpl.
-  destruct (MooreSemantics.initialState m), (initialState (execute Moore2Mealy m)).
-  reflexivity. reflexivity. reflexivity. reflexivity.
-- destruct (MooreSemantics.initialState m), (initialState (execute Moore2Mealy m)).
-  simpl.
-Admitted. 
+intros.
+unfold Moore_execute, Mealy_execute.
+destruct (MooreSemantics.initialState m) eqn: q0.
++ destruct (initialState (execute Moore2Mealy m)) eqn: q0_tr.
+  ++ revert q0. revert s. revert q0_tr. revert s0.
+     induction i.
+     - intros. simpl. auto.
+     - intros.
+  ++ (* contradiction *)
+     (* need uniqueness on initial state *)
+     admit.
++ destruct (initialState (execute Moore2Mealy m)) eqn: q0_tr.
+  ++ (* contradiction *)
+     admit.
+  ++ auto.
+Admitted.
+
