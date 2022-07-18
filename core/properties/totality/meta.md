84db86f5ecd8ebcb2aaa50945204ea6f8a57628d
Zheng Cheng <cz0590@gmail.com>
2022-07-18 13:52:24 +0200

    add totality

---
+++ core/properties/totality/meta.md
@@ -0,0 +1,8 @@
+
+* 7/18 12:30 pm - 1:30 pm (1:00h)
+  * work on totality
+    * prepare theorem model/link
+      * renew def
+    * proof
+      * by engine lemma
+
---
+++ core/properties/totality/totality.v
@@ -0,0 +1,56 @@
+Require Import String.
+Require Import EqNat.
+Require Import List.
+Require Import PeanoNat.
+Require Import Lia.
+Require Import FunctionalExtensionality.
+
+Require Import core.Semantics.
+Require Import core.Syntax.
+Require Import core.Model.
+Require Import core.TransformationConfiguration.
+Require Import core.Certification.
+Require Import core.utils.Utils.
+
+Require Import core.modeling.ConcreteSyntax.
+Require Import core.modeling.ModelingSemantics.
+Require Import core.modeling.ModelingMetamodel.
+Require Import core.modeling.ConcreteExpressions.
+Require Import core.modeling.Parser.
+
+Require Import transformations.Moore2Mealy.Moore.
+Require Import transformations.Moore2Mealy.Moore2Mealy.
+Require Import transformations.Moore2Mealy.tests.sampleMoore_injective.
+
+
+(*************************************************************)
+(** * Totality of CoqTL                                      *)
+(*************************************************************)
+
+(** Totality on model elements                               *)
+
+Theorem Totality_elem:
+forall (tr: Transformation) (sm : SourceModel) (sp : list SourceModelElement) (te : TargetModelElement),
+In sp (allTuples tr sm) -> 
+In te (instantiatePattern tr sm sp) ->
+In te (allModelElements (execute tr sm)).
+Proof.
+    intros.
+    apply tr_execute_in_elements.
+    eexists sp. 
+    auto.
+Qed.
+
+(** Totality on model links                                  *)
+
+Theorem Totality_link:
+forall (tr: Transformation) (sm : SourceModel) (sp : list SourceModelElement) (tl : TargetModelLink),
+In sp (allTuples tr sm) -> 
+In tl (applyPattern tr sm sp) ->
+In tl (allModelLinks (execute tr sm)).
+Proof.
+    intros.
+    apply tr_execute_in_links.
+    eexists sp. 
+    auto.
+Qed.
