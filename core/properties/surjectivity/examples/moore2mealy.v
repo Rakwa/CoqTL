8ff8ecbc668ee72ad8cfc075ea256a9512315381
Zheng Cheng <cz0590@gmail.com>
2022-08-01 15:51:00 +0200

    Create moore2mealy.v

---
+++ core/properties/surjectivity/examples/moore2mealy.v
@@ -0,0 +1,81 @@
+Require Import String.
+Require Import EqNat.
+Require Import List.
+Require Import PeanoNat.
+Require Import Lia.
+Require Import FunctionalExtensionality.
+
+Require Import core.Semantics.
+Require Import core.Syntax.
+Require Import core.Model.
+Require Import core.TransformationConfiguration.
+Require Import core.Certification.
+Require Import core.utils.Utils.
+
+Require Import core.modeling.ConcreteSyntax.
+Require Import core.modeling.ModelingSemantics.
+Require Import core.modeling.ModelingMetamodel.
+Require Import core.modeling.ConcreteExpressions.
+Require Import core.modeling.Parser.
+
+Require Import transformations.Moore2Mealy.Moore.
+Require Import transformations.Moore2Mealy.Mealy.
+Require Import transformations.Moore2Mealy.Moore2Mealy.
+
+Require Import core.properties.surjectivity.surjectivity.
+
+
+(*************************************************************)
+(** * Surjectivity of CoqTL                                  *)
+(*************************************************************)
+
+(** directly instantiate Surjectivity on Moore2Mealy              *)
+
+Theorem Moore2Mealy_Surjectivity_elem_test :
+forall (sm : MooreModel) (te : MealyMetamodel_Object),
+      In te (allModelElements (execute Moore2Mealy sm)) ->
+      (exists (sp : list MooreMetamodel_Object),
+          In sp (allTuples Moore2Mealy sm) /\
+          In te (instantiatePattern Moore2Mealy sm sp)).
+Proof.
+    apply Surjectivity_elem.
+Qed.
+
+
+(** apply Surjectivity on more realistic user theorem             *)
+
+
+Theorem Moore2Mealy_Surjectivity_elem_test3 :
+forall (sm : MooreModel) (te : Mealy.State),
+    let mealystate := (MealyMetamodel_toObject Mealy.StateClass te) in
+      In mealystate (allModelElements (execute Moore2Mealy sm)) ->
+      (exists (se: Moore.State),
+          let moorestate := (MooreMetamodel_toObject Moore.StateClass se) in
+          In [moorestate] (allTuples Moore2Mealy sm) /\
+          In mealystate (instantiatePattern Moore2Mealy sm [moorestate])).
+Proof.
+intros.
+apply Surjectivity_elem in H.
+destruct H.
+destruct x as [nil | o] eqn: sp_ca.
+- (* sp = [] *)
+  crush.
+- (* sp != [] *)
+  destruct o as [tp e].
+  destruct tp. 
+  + (* sp = [state; sth] *)
+    destruct l eqn: sp_rest.
+    ++ simpl in H. crush. exists e. crush.
+    ++ simpl in H. crush.
+  + (* sp = [transition; sth] *)
+    destruct l eqn: sp_rest.
+    ++ simpl in H. crush. 
+    unfold toModelElement in H.
+    unfold toSumType in H.
+    simpl in H.
+    unfold mealystate in H.
+    inversion H.
+    ++ simpl in H. crush.
+Qed.
+
+
