724c6ac481720c49890cca8fbeb360683d083ab0
julien <julien.raquois@gmail.com>
2022-11-25 15:02:00 +0100

    order table

--- transformations/TT2BDDSimple/TT2BDD.v
+++ transformations/TT2BDDSimple/TT2BDD.v
@@ -2,6 +2,7 @@ Require Import String.
 Require Import List.
 Require Import Multiset.
 Require Import ListSet.
+Require Import Bool.
 
 Require Import core.utils.Utils.
 
@@ -20,6 +21,63 @@ Check bddnode.
 Check bddnil.
 Check BuildTable2.
 
+
+(* Append nat to end of list *)
+Fixpoint addToEnd (r: nat) (l: list nat): list nat :=
+    match l with
+    | h::t => h::(addToEnd r t)
+    | nil => r::nil
+    end.
+
+(* Append string to end of list *)
+Fixpoint addToEndString (r: string) (l: list string): list string :=
+    match l with
+    | h::t => h::(addToEndString r t)
+    | nil => r::nil
+    end.
+
+(* Move first column input to end of list *)
+Definition moveFirstColumnInput (inputs: list string) := 
+    match inputs with
+    | nil => nil
+    | firstColumn::otherColums => addToEndString firstColumn otherColums
+    end.
+
+(*Compute moveFirstColumnInput ("a"::"b"::"c"::nil).*)
+
+(* Move first column of each row to end of list *)
+Fixpoint moveFirstColumnRows (rows: list (prod (list nat) nat)) := 
+    match rows with
+    | nil => nil
+    | (nil, output)::otherRows => (nil, output)::otherRows
+    | (firstColumn::otherColums, output)::otherRows => ((addToEnd firstColumn otherColums), output) :: moveFirstColumnRows(otherRows)
+    end.
+
+(* Compute moveFirstColumnRows (((0::0::nil),1)::((0::1::nil),0)::nil). *)
+    
+(* check if multiple value in the first column of each row *)
+Fixpoint isMultipleValueFirstColumn (rows: list (prod (list nat) nat)) : bool := 
+    match rows with 
+    | nil => false
+    | (nil, _) :: _=> false
+    | (firstColumn::otherColums, _)::othersRows => 
+        match firstColumn with 
+        | 2 => true
+        | _ => isMultipleValueFirstColumn(othersRows)
+        end 
+    end.
+
+(*Compute isMultipleValueFirstColumn (((0::0::nil),1)::((2::1::nil),0)::nil).
+Compute isMultipleValueFirstColumn (((0::0::nil),1)::((1::2::nil),0)::nil).*)
+ 
+(* order tt to have multiple value in the last columns *)    
+Fixpoint orderTT (inputsSize: nat) (inputs: list string) (rows: list (prod (list nat) nat)) : prod (list string) (list (prod (list nat) nat)) :=
+    match inputsSize with
+    | 0 => (inputs, rows)
+    | (S u) => if (isMultipleValueFirstColumn(rows)) then (orderTT (u) (moveFirstColumnInput inputs) (moveFirstColumnRows rows)) else (inputs, rows)
+    end.
+
+(* filter matrice with false value in firstColumn *)
 Fixpoint getFalseTableWithoutFirstColumn(matrice: list (prod (list nat) nat)): list (prod (list nat) nat) :=
     match matrice with
     | (0::inputsValue, outputValue)::rows => (inputsValue, outputValue) :: getFalseTableWithoutFirstColumn(rows)
@@ -28,6 +86,7 @@ Fixpoint getFalseTableWithoutFirstColumn(matrice: list (prod (list nat) nat)): l
     | nil => nil
     end.
 
+(* filter matrice with true value in firstColumn *)
 Fixpoint getTrueTableWithoutFirstColumn(matrice: list (prod (list nat) nat)): list (prod (list nat) nat) :=
     match matrice with
     | (1::inputsValue, outputValue)::rows => (inputsValue, outputValue) :: getTrueTableWithoutFirstColumn(rows)
@@ -36,24 +95,30 @@ Fixpoint getTrueTableWithoutFirstColumn(matrice: list (prod (list nat) nat)): li
     | nil => nil
     end.
 
-    
-Fixpoint getBDDFromTT (inputs: list string) (output: string) (rows: list (prod (list nat) nat)) : BDDTree := 
-    match rows with 
-    | nil => bddnil
-    | ((_,outputValue)::nil) => bddleaf output outputValue
-    | rows => 
-        match inputs with
+(* transform tt to bdd *)    
+Fixpoint getBDDFromTT (inputsLength: nat)  (inputs: list string) (output: string) (rows: list (prod (list nat) nat)) : BDDTree := 
+    match orderTT inputsLength inputs rows with 
+    | (orderInputs, orderRows) => 
+        match orderRows with 
         | nil => bddnil
-        | (firstInput::otherInputs) => bddnode firstInput 
-                                        (getBDDFromTT otherInputs output (getFalseTableWithoutFirstColumn rows))  
-                                        (getBDDFromTT otherInputs output (getTrueTableWithoutFirstColumn rows))
+        | ((_,outputValue)::nil) => bddleaf output outputValue
+        | rows => 
+            match orderInputs with
+            | nil => bddnil
+            | (firstInput::otherInputs) => 
+                match inputsLength with 
+                | 0 => bddnil
+                | (S u) => bddnode firstInput 
+                            (getBDDFromTT u otherInputs output (getFalseTableWithoutFirstColumn rows))
+                            (getBDDFromTT u otherInputs output (getTrueTableWithoutFirstColumn rows))
+                end
+            end
         end
     end.
 
-
 Definition TT2BDD (t: Table2) := 
     match t with 
-    | BuildTable2 inputs output rows => getBDDFromTT inputs output rows
+    | BuildTable2 inputs output rows => getBDDFromTT (length inputs) inputs output rows
     end.
 
 
